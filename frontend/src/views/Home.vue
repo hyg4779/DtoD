7e1c47510611a3af7c8067cd13c0d3c349ee9a75
LEE DONG CHUL <sok8079@naver,com>
2022-01-17 18:21:53 +0900

    FE_navbar_main

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,72 @@
 <template>
   <div class="home">
-    홈페이지
+    <div>
+      <q-carousel
+        class="carousel"
+        animated
+        v-model="slide"
+        navigation
+        infinite
+        :autoplay="autoplay"
+        arrows
+        transition-prev="slide-right"
+        transition-next="slide-left"
+        @mouseenter="autoplay = false"
+        @mouseleave="autoplay = true"
+      >
+        <q-carousel-slide :name="1" class="slide1">
+          <div>
+            <span>
+              같이 공부할 친구를 찾는 것은,
+              <br>
+              프로젝트보다 만들기 어렵습니다.
+            </span>
+          </div>
+          <img src="../assets/carousel1.png" alt="1">
+        </q-carousel-slide>
+        <q-carousel-slide :name="2" class="slide2">
+        </q-carousel-slide>
+        <q-carousel-slide :name="3" class="slide3">
+        </q-carousel-slide>
+      </q-carousel>
+    </div>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 
 export default {
   name: 'Home',
   components: {
 
+  },
+  setup () {
+    return {
+      slide: ref(1),
+      // autoplay: ref(true)
+    }
   }
 }
 </script>
+
+<style>
+.carousel {
+  
+}
+
+.slide1 {
+  display: flex;
+  justify-content: space-between;
+  background-color: #DDF1FF;
+  margin: 0;
+  padding: 0 !important;
+}
+
+.slide1 > img {
+  /* float: right; */
+  width: 35%;
+  height: 100%;
+}
+
+</style>
--- frontend/src/views/Navbar.vue
+++ frontend/src/views/Navbar.vue
@@ -1,56 +1,56 @@
 <template>
-  <div id="nav">
-    <div class="menubar">
-      <div class="logo">
+  <div class="menubar">
+    <div class="logo">
+      <a href="/">
         <span>DtoD</span>
-      </div>
-      <ul class="menu">
-        <li @mouseover="listOne = true" @mouseleave="listOne = false">
-          <a href="#">스터디룸</a>
-          <transition name="fade">
-            <ul v-if="listOne" @click="listOne = false">
-              <li><a href="#">진행중</a></li>
-              <li><a href="#">스터디 만들기</a></li>
-            </ul>        
-          </transition>     
-        </li>     
-        <li @mouseover="listTwo = true" @mouseleave="listTwo = false">   
-          <a href="#">게시판</a>
-          <transition name="fade">        
-            <ul v-if="listTwo" @click="listTwo = false">
-              <li><a href="#">스터디 모집</a></li>
-              <li><a href="#">자유 게시판</a></li>
-            </ul>
-          </transition>
-        </li>
-        <li @mouseover="listThree = true" @mouseleave="listThree = false">
+      </a>
+    </div>
+    <ul class="menu">
+      <li @mouseover="listOne = true" @mouseleave="listOne = false">
+        <a href="#">스터디룸</a>
+        <transition name="fade">
+          <ul v-if="listOne" @click="listOne = false">
+            <li><a href="#">진행중</a></li>
+            <li><a href="#">스터디 만들기</a></li>
+          </ul>        
+        </transition>     
+      </li>     
+      <li @mouseover="listTwo = true" @mouseleave="listTwo = false">   
+        <a href="#">게시판</a>
+        <transition name="fade">        
+          <ul v-if="listTwo" @click="listTwo = false">
+            <li><a href="#">스터디 모집</a></li>
+            <li><a href="#">자유 게시판</a></li>
+          </ul>
+        </transition>
+      </li>
+      <li @mouseover="listThree = true" @mouseleave="listThree = false">
           <q-avatar class="profile">
             <img src="../assets/default_user.png">
           </q-avatar>
-          <transition name="fade">         
-            <ul v-if="listThree" @click="listThree = false">
-              <!-- <div v-if="logined">
-                <li><a href="#">Sub Menu Item</a></li>
-                <li><a href="#">Sub Menu Item</a></li>
-                <li><a href="#">Sub Menu Item</a></li> 
-              </div> -->
-              <!-- <div v-else> -->
-              <div>
-                <li>
-                  <a href="#">
-                    <router-link :to="{ name:'Login' }">
-                      로그인을
-                      <br>
-                      해주세요
-                    </router-link>
-                  </a>
-                </li>
-              </div>
-            </ul>
-          </transition>
-        </li>
-      </ul>
-    </div>
+        <transition name="fade">         
+          <ul v-if="listThree" @click="listThree = false">
+            <!-- <div v-if="logined">
+              <li><a href="#">Sub Menu Item</a></li>
+              <li><a href="#">Sub Menu Item</a></li>
+              <li><a href="#">Sub Menu Item</a></li> 
+            </div> -->
+            <!-- <div v-else> -->
+            <div>
+              <li>
+                <a href="#">
+                  <router-link :to="{ name:'Login' }">
+                    로그인을
+                    <br>
+                    해주세요
+                  </router-link>
+                </a>
+              </li>
+            </div>
+          </ul>
+        </transition>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -72,6 +72,16 @@ export default {
 </script>
 
 <style>
+@font-face {
+  font-family: "Dohyeon";
+  src: url("../assets/font/BMDOHYEON_ttf.ttf");
+}
+
+@font-face {
+  font-family: "Hanna";
+  src: url("../assets/font/BMHANNAPro.ttf");
+}
+
 .menubar {
   display: flex;
   justify-content: space-between;
@@ -79,18 +89,29 @@ export default {
 
 .logo {
   font-size: 40px;
-  font-weight: bold;
-  margin: auto 10px;
+  margin: auto 35px;
   color: #24274A;
 }
 
+.logo a {
+  color: #24274A;
+  text-decoration: none;
+  font-family: 'Dohyeon', sans-serif;
+}
 
 .menu {  
-  font: 14px/1.5 'Open Sans', sans-serif;
-  font-weight: 600;
+  font: 14px/1.5 'Roboto', sans-serif;
   margin: 0 3px 0 0;
   padding: 0;
   list-style: none;
+}
+
+.menu li {
+  display:block;
+  float: left;
+  position: relative;
+  min-width: 150px;
+  font-size: 20px;
 }
 
 .menu a {
@@ -99,20 +120,13 @@ export default {
   color: #616DCC;
   text-decoration: none;
   text-align: center;
-}
-
- .menu li {
-  display:block;
-  float: left;
-  position: relative;
-  min-width: 150px;
-  font-size: 15px;
+  font-weight: bold;  
 }
 
 .menu li .profile {
   display:block;
-  margin: 6.5px auto;
-  padding: auto;
+  margin: 11px auto;
+  padding: 0;
 }
 
 .menu li ul {
@@ -121,10 +135,13 @@ export default {
   top: 61px;
   margin: 0;
   padding: 0;
+  border-radius: 10px;
   box-shadow: 3px 3px 3px rgb(192, 192, 192);
+  z-index: 1;
 }
 
 .menu li ul li {
+  font-size: 15px;
   background: rgb(255, 255, 255);
   transition: background .2s;
 }
