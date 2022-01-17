ce923c86c73af050c1614d5c99a5d88f5ec7ff21
comkkyu <bakha1105@gmail.com>
2022-01-17 16:54:23 +0900

    백엔드 환경 설정

--- backend/src/main/java/com/ssafy/dtod/controller/UserController.java
+++ backend/src/main/java/com/ssafy/dtod/controller/UserController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-import javax.persistence.criteria.Join;
-
 @RestController
 @RequestMapping("/api")
 @Api(tags = {"회원관리 API"})
@@ -21,5 +19,23 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    
+    @PostMapping("/user/join")
+    @ApiOperation(value = "사용자 등록", notes = "이름, 비밀번호로 유저를 생성한다.")
+    public User create(@RequestBody User user) {
+        System.out.println("사용자 등록 START");
+        System.out.println(user.getEmail());
+        System.out.println(user.getUsername());
+        System.out.println(user.getPassword());
+        System.out.println("사용자 등록 END");
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/user/{email}")
+    @ApiOperation(value = "사용자 조회", notes = "email에 해당하는 유저정보를 조회한다.")
+    public User read(@PathVariable String email) {
+        Optional<User> userOptional = userRepository.findByEmail(email);
+        userOptional.ifPresent(System.out::println);
+
+        return userOptional.get();
+    }
 }
--- backend/src/main/java/com/ssafy/dtod/model/User.java
+++ backend/src/main/java/com/ssafy/dtod/model/User.java
@@ -2,18 +2,14 @@ package com.ssafy.dtod.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String email;
 
     private String username;
 
