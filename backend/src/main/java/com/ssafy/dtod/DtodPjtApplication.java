6aa372ea9ac2267e98b6232ab7749093db9328ea
SSAFY <elston0425@naver.com>
2022-01-17 13:52:02 +0900

    backend

--- backend/src/main/java/com/ssafy/dtod/DtodPjtApplication.java
+++ backend/src/main/java/com/ssafy/dtod/DtodPjtApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.ssafy.dtod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebRtcPjtApplication {
+public class DtodPjtApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebRtcPjtApplication.class, args);
+		SpringApplication.run(DtodPjtApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/ssafy/dtod/DtodPjtApplicationTests.java
+++ backend/src/test/java/com/ssafy/dtod/DtodPjtApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.ssafy.dtod;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WebRtcPjtApplicationTests {
+class DtodPjtApplicationTests {
 
 	@Test
 	void contextLoads() {
