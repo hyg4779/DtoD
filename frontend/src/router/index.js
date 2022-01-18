1407b50f30b4ba7e5bedde9eb7a0d38b1a0beeeb
hong <hyg4779@naver.com>
2022-01-18 19:17:07 +0900

    login modal correcting error

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
 import Signup from '../views/Signup.vue'
 
 const routes = [
@@ -9,11 +8,7 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
+
   {
     path: '/signup',
     name: 'Signup',
