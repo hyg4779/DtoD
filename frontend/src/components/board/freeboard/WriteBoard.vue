80cd375d8e8895227740c28d3f26bf10ed91f013
LEE DONG CHUL <sok8079@naver,com>
2022-01-25 23:20:40 +0900

    FE_css change

--- frontend/src/components/board/freeboard/WriteBoard.vue
+++ frontend/src/components/board/freeboard/WriteBoard.vue
@@ -3,97 +3,75 @@
     <div class="title">
       게시글 작성
     </div>
-    <!-- <form> -->
-    <!-- <form @submit="onSubmit"> -->
-      <!-- <div>
+    <form @submit="onSubmit">
+      <div class="form-group">
         <label for="title">제목</label>
         <br>
         <textarea class="form-control" type="text" id="title" v-model="title" placeholder=" 제목을 입력하세요"></textarea>
       </div>
-      <div class="form-control">
-        <span>기술 스택 및 협업 툴</span>
+      <div class="form-group">
+        <div class="stacktitle">기술 스택 및 협업 툴</div>
         <div class="checkbox">
           <div>
-            <q-checkbox v-model="stacks.javascript" label="JavaScript"/>
-            <q-checkbox v-model="stacks.react" label="React.js"/>
-            <q-checkbox v-model="stacks.vue" label="Vue.js"/>
-            <q-checkbox v-model="stacks.node" label="Node.js"/>
+            <v-checkbox v-model="stacks.javascript" label="JavaScript"/>
+            <v-checkbox v-model="stacks.react" label="React.js"/>
+            <v-checkbox v-model="stacks.vue" label="Vue.js"/>
+            <v-checkbox v-model="stacks.node" label="Node.js"/>
           </div>
           <div>
-            <q-checkbox v-model="stacks.c" label="C"/>
-            <q-checkbox v-model="stacks.cpp" label="C++"/>
-            <q-checkbox v-model="stacks.cs" label="C#"/>
-            <q-checkbox v-model="stacks.typescript" label="TypeScript.js"/>
+            <v-checkbox v-model="stacks.c" label="C"/>
+            <v-checkbox v-model="stacks.cpp" label="C++"/>
+            <v-checkbox v-model="stacks.cs" label="C#"/>
+            <v-checkbox v-model="stacks.typescript" label="TypeScript.js"/>
           </div>
           <div>
-            <q-checkbox v-model="stacks.kotlin" label="Kotlin"/>
-            <q-checkbox v-model="stacks.django" label="Django"/>
-            <q-checkbox v-model="stacks.go" label="Go"/>
-            <q-checkbox v-model="stacks.swift" label="Swift"/>
+            <v-checkbox v-model="stacks.kotlin" label="Kotlin"/>
+            <v-checkbox v-model="stacks.django" label="Django"/>
+            <v-checkbox v-model="stacks.go" label="Go"/>
+            <v-checkbox v-model="stacks.swift" label="Swift"/>
           </div>
           <div>
-            <q-checkbox v-model="stacks.java" label="Java"/>
-            <q-checkbox v-model="stacks.spring" label="Spring"/>
-            <q-checkbox v-model="stacks.flutter" label="Flutter"/>
-            <q-checkbox v-model="stacks.etc" label="Etc"/>
+            <v-checkbox v-model="stacks.java" label="Java"/>
+            <v-checkbox v-model="stacks.spring" label="Spring"/>
+            <v-checkbox v-model="stacks.flutter" label="Flutter"/>
+            <v-checkbox v-model="stacks.etc" label="Etc"/>
           </div>
         </div>
       </div>
-      <div class="detail1">
-         <label for="content">내용</label>
+      <div class="detail1 form-group">
+        <label for="content">내용</label>
         <br>
         <textarea class="form-control" type="text" id="content" v-model="content" placeholder=" 내용를 입력하세요"></textarea>
       </div>
-      <div class="detail2">
+      <div class="detail2 form-group">
         <label for="code">코드 입력</label>
         <br>
         <textarea class="form-control" type="text" id="code" v-model="code" placeholder=" 코드를 입력하세요"></textarea>
       </div>
       <div class="submitbtn">
-        <button class="cancle" @click="back()">취소</button>
+        <button class="cancel" @click="back()">취소</button>
         <button class="submit">등록</button>
       </div>
-    </form> -->
+    </form>
   </div>
 </template>
 
 <script>
-// import { reactive } from 'vue'
 // import axios from 'axios'
 
 export default {
   name: 'WriteBoard',
-  components: {
-    
-  },
-  // setup () {
-  //   const stacks = reactive({
-  //       javascript: false,
-  //       c: false,
-  //       kotlin: false,
-  //       java: false,
-  //       react: false,
-  //       cpp: false,
-  //       django: false,
-  //       spring: false,
-  //       vue: false,
-  //       cs: false,
-  //       go: false,
-  //       flutter: false,
-  //       node: false,
-  //       typescript: false,
-  //       swift: false,
-  //       etc: false,
-  //     })
-  //   return {
-  //     stacks,
-  //   }
-  // },
+
   data () {
     return {
       title: '',
       content: '',
       code: '',
+      stacks: {
+        javascript: false, c: false, kotlin: false, java: false, 
+        react: false, cpp: false, django: false, spring: false, 
+        vue: false, cs: false, go: false, flutter: false, 
+        node: false, typescript: false, swift: false, etc: false},
       result: [],
     }
   },
@@ -150,15 +128,18 @@ form {
 form div label {
   font-weight: bold;
   font-size: 20px;
+  margin: 0 0 10px 0;
 }
 
-form div span {
+form div .stacktitle {
   font-weight: bold;
   font-size: 20px;
+  margin: 0 0 10px 0;
 }
 
 form div textarea {
   margin: 0 0 20px 0;
+  padding: 0;
   border: 0.1px solid #C4C4C4;
   border-radius: 10px;
   height: 40px;
@@ -185,17 +166,18 @@ form .detail2 textarea {
 form .checkbox {
   margin: 0 0 20px 0;
   display: grid;
-  grid-template-columns: 150px 150px 150px 150px;
+  grid-template-columns: 200px 200px 200px 200px;
 }
+
 form .checkbox div{
-  margin-right: 20px;
+  margin: 0;
 }
 
 .submitbtn {
   margin: 0 0 30px 0;
 }
 
-.submitbtn .cancle {
+.submitbtn .cancel {
   cursor: pointer;
   font-family: 'Roboto';
   font-size: 16px;
--- frontend/src/components/board/studyboard/WriteBoard.vue
+++ frontend/src/components/board/studyboard/WriteBoard.vue
@@ -3,99 +3,81 @@
     <div class="title">
       스터디 모집
     </div>
-    <!-- <form @submit="onSubmit">
-      <div>
+    <form @submit="onSubmit">
+      <div class="form-group">
         <label for="title">제목</label>
         <br>
         <textarea class="form-control" type="text" id="title" v-model="title" placeholder=" 제목을 입력하세요"></textarea>
       </div>
-      <div class="form-control">
-        <span>기술 스택 및 협업 툴</span>
+      <div class="form-group">
+        <div class="stacktitle">기술 스택 및 협업 툴</div>
         <div class="checkbox">
           <div>
-            <q-checkbox v-model="stacks.javascript" label="JavaScript"/>
-            <q-checkbox v-model="stacks.react" label="React.js"/>
-            <q-checkbox v-model="stacks.vue" label="Vue.js"/>
-            <q-checkbox v-model="stacks.node" label="Node.js"/>
+            <v-checkbox v-model="stacks.javascript" label="JavaScript"/>
+            <v-checkbox v-model="stacks.react" label="React.js"/>
+            <v-checkbox v-model="stacks.vue" label="Vue.js"/>
+            <v-checkbox v-model="stacks.node" label="Node.js"/>
           </div>
           <div>
-            <q-checkbox v-model="stacks.c" label="C"/>
-            <q-checkbox v-model="stacks.cpp" label="C++"/>
-            <q-checkbox v-model="stacks.cs" label="C#"/>
-            <q-checkbox v-model="stacks.typescript" label="TypeScript.js"/>
+            <v-checkbox v-model="stacks.c" label="C"/>
+            <v-checkbox v-model="stacks.cpp" label="C++"/>
+            <v-checkbox v-model="stacks.cs" label="C#"/>
+            <v-checkbox v-model="stacks.typescript" label="TypeScript.js"/>
           </div>
           <div>
-            <q-checkbox v-model="stacks.kotlin" label="Kotlin"/>
-            <q-checkbox v-model="stacks.django" label="Django"/>
-            <q-checkbox v-model="stacks.go" label="Go"/>
-            <q-checkbox v-model="stacks.swift" label="Swift"/>
+            <v-checkbox v-model="stacks.kotlin" label="Kotlin"/>
+            <v-checkbox v-model="stacks.django" label="Django"/>
+            <v-checkbox v-model="stacks.go" label="Go"/>
+            <v-checkbox v-model="stacks.swift" label="Swift"/>
           </div>
           <div>
-            <q-checkbox v-model="stacks.java" label="Java"/>
-            <q-checkbox v-model="stacks.spring" label="Spring"/>
-            <q-checkbox v-model="stacks.flutter" label="Flutter"/>
-            <q-checkbox v-model="stacks.etc" label="Etc"/>
+            <v-checkbox v-model="stacks.java" label="Java"/>
+            <v-checkbox v-model="stacks.spring" label="Spring"/>
+            <v-checkbox v-model="stacks.flutter" label="Flutter"/>
+            <v-checkbox v-model="stacks.etc" label="Etc"/>
           </div>
         </div>
       </div>
-      <div class="detail1">
+      <div class="detail1 form-group">
          <label for="content1">오픈 카카오톡 (연락처)</label>
         <br>
         <textarea class="form-control" type="text" id="content1" v-model="content1" placeholder=" 연락처를 입력하세요"></textarea>
       </div>
-      <div class="detail2">
+      <div class="detail2 form-group">
          <label for="content2">스터디 소개</label>
         <br>
         <textarea class="form-control" type="text" id="content2" v-model="content2" placeholder=" 스터디 소개를 입력하세요"></textarea>
       </div>
-      <div class="detail3">
+      <div class="detail3 form-group">
         <label for="content3">스터디 규칙</label>
         <br>
         <textarea class="form-control" type="text" id="content3" v-model="content3" placeholder=" 스터디 규칙을 입력하세요"></textarea>
       </div>
       <div class="submitbtn">
-        <button class="cancle" @click="back()">취소</button>
+        <button class="cancel" @click="back()">취소</button>
         <button class="submit">등록</button>
       </div>
-    </form> -->
+    </form>
   </div>
 </template>
 
 <script>
-// import { reactive } from 'vue'
 // import axios from 'axios'
 
 export default {
   name: 'WriteBoard',
-  // setup () {
-  //   const stacks = reactive({
-  //       javascript: false,
-  //       c: false,
-  //       kotlin: false,
-  //       java: false,
-  //       react: false,
-  //       cpp: false,
-  //       django: false,
-  //       spring: false,
-  //       vue: false,
-  //       cs: false,
-  //       go: false,
-  //       flutter: false,
-  //       node: false,
-  //       typescript: false,
-  //       swift: false,
-  //       etc: false,
-  //     })
-  //   return {
-  //     stacks,
-  //   }
-  // },
+
   data () {
     return {
       title: '',
       content1: '',
       content2: '',
       content3: '',
+      stacks: {
+        javascript: false, c: false, kotlin: false, java: false, 
+        react: false, cpp: false, django: false, spring: false, 
+        vue: false, cs: false, go: false, flutter: false, 
+        node: false, typescript: false, swift: false, etc: false},
       result: [],
     }
   },
@@ -153,15 +135,18 @@ form {
 form div label {
   font-weight: bold;
   font-size: 20px;
+  margin: 0 0 10px 0;
 }
 
-form div span {
+form div .stacktitle {
   font-weight: bold;
   font-size: 20px;
+  margin: 0 0 10px 0;
 }
 
 form div textarea {
   margin: 0 0 20px 0;
+  padding: 0;
   border: 0.1px solid #C4C4C4;
   border-radius: 10px;
   height: 40px;
@@ -200,14 +185,14 @@ form .checkbox {
   grid-template-columns: 150px 150px 150px 150px;
 }
 form .checkbox div{
-  margin-right: 20px;
+  margin: 0;
 }
 
 .submitbtn {
   margin: 0 0 10px 0;
 }
 
-.submitbtn .cancle {
+.submitbtn .cancel {
   cursor: pointer;
   font-family: 'Roboto';
   font-size: 16px;
