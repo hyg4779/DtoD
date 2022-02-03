c036306576c8b4e9bdbc54bf3074bba569d22712
comkkyu <bakha1105@gmail.com>
2022-02-03 13:32:37 +0900

    fix: BE_빌드 에러 해결

--- backend/src/test/java/com/ssafy/dtod/DtodPjtApplicationTests.java
+++ backend/src/test/java/com/ssafy/dtod/DtodPjtApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DtodPjtApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
