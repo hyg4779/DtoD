255e135875b51e4530163b930533ac08bb17fd46
LEE DONG CHUL <sok8079@naver,com>
2022-02-06 20:52:09 +0900

    feat: FE_userImg

--- frontend/src/components/profile/MyInfo.vue
+++ frontend/src/components/profile/MyInfo.vue
@@ -5,8 +5,12 @@
         <img src="../../assets/default_user.png">
       </div>
       <div class="profilebutton">
-        <button class="imgadd" @click="selectUploadFile()">이미지 등록</button>
+        <!-- <button class="imgadd" @click="selectUploadFile()">이미지 등록</button> -->
+        <label for="profileimg">이미지 선택</label>
+        <input id="profileimg" type="file" accept="image/*" @change="previewImage" /> 
+        <button class="imgadd"  @click="onUpload">이미지 등록</button>
       </div>
+      <div class="progress">업로드: {{ uploadValue.toFixed() + "%" }}</div>
     </div>
     <b-form class="form">
       <label class="nicknamelabel" for="nickname">닉네임</label>
@@ -64,15 +68,20 @@
 </template>
 
 <script>
-import axios from 'axios'
-import { api } from '../../../api.js'
+// import axios from 'axios'
+// import { api } from '../../../api.js'
 // import jwt_decode from 'jwt-decode'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/storage'
 
 export default {
     name: 'MyInfo',
     data() {
       return {
-        img: '',
+        uploadValue : 0,
+        picture: null,
+        imageData: null,
+
         stacks: {
           javascript: false,
           c: false,
@@ -91,6 +100,7 @@ export default {
           swift: false,
           etc: false
         },
+
         options: [
           '웹 프로그래머',
           '웹 퍼블리셔',
@@ -100,7 +110,9 @@ export default {
           '데이터 엔지니어',
           '데브옵스 엔지니어',
         ],
+
         credentials: {
+          img: '',
           nickname: '',
           jobs: null,
           skills: [],
@@ -108,41 +120,61 @@ export default {
       }
     },
     methods: {
-      selectUploadFile() {
-        var vue = this
-        let elem = document.createElement('input')
-        // 이미지 파일 업로드 / 동시에 여러 파일 업로드
-        elem.id = 'image'
-        elem.type = 'file'
-        // elem.accept = 'image/*'
-        elem.accept = ['image/png', 'image/jpeg']
-        elem.multiple = false
-        // 클릭
-        elem.click();
-        // 이벤트 감지
-        elem.onchange = function() {
-          const formData = new FormData()
-
-          formData.append('file', this.files[0])
-
-          const token = localStorage.getItem('jwt')
-          axios({
-            method: 'post',
-            url: api.USER_INFO_CHANGE,
-            data: formData,
-            headers: { 
-              'Content-Type': 'multipart/form-data' ,
-              Authorization: 'Bearer ' + token
-            }
-          }).then(response => {
-              vue.response = response.data
-          }).catch(error => {
-              vue.response = error.message
-          })
+      previewImage(event) { 
+        this.uploadValue=0; 
+        this.picture=null; 
+        this.imageData = event.target.files[0]; 
+      }, 
+      onUpload( ){ 
+        this.picture=null; 
+        const storageRef = firebase.storage().ref(`${this. imageData.name}`).put(this.imageData); 
+        storageRef.on( `state_changed` ,snapshot=>{ 
+          this.uploadValue = (snapshot.bytesTransferred/snapshot.totalBytes)*100; 
+        }, error=>{console.log(error.message)}, 
+        ()=>{this.uploadValue=100; 
+        storageRef.snapshot.ref.getDownloadURL().then((url)=>{ 
+          this.picture =url; 
+          // console.log(url)
+          }); 
         }
-      },
-      // }
+        )
+      }
     },
+    // methods: {
+    //   selectUploadFile() {
+    //     var vue = this
+    //     let elem = document.createElement('input')
+    //     // 이미지 파일 업로드 / 동시에 여러 파일 업로드
+    //     elem.id = 'image'
+    //     elem.type = 'file'
+    //     // elem.accept = 'image/*'
+    //     elem.accept = ['image/png', 'image/jpeg']
+    //     elem.multiple = false
+    //     // 클릭
+    //     elem.click();
+    //     // 이벤트 감지
+    //     elem.onchange = function() {
+    //       const formData = new FormData()
+
+    //       formData.append('file', this.files[0])
+
+    //       const token = localStorage.getItem('jwt')
+    //       axios({
+    //         method: 'post',
+    //         url: api.USER_INFO_CHANGE,
+    //         data: formData,
+    //         headers: { 
+    //           'Content-Type': 'multipart/form-data' ,
+    //           Authorization: 'Bearer ' + token
+    //         }
+    //       }).then(response => {
+    //           vue.response = response.data
+    //       }).catch(error => {
+    //           vue.response = error.message
+    //       })
+    //     }
+    //   },
+    // },
     created() {
         // token에서 유저 상세 정보 뺴옴
         // const token = localStorage.getItem('jwt')
@@ -185,17 +217,52 @@ export default {
 .profilebutton{
   display: flex;
 }
-.imgadd {
+
+.profilebutton label {
+  display: inline-block;
+  line-height: normal; 
+  vertical-align: middle;
+  text-align: center;
+  padding: 0.8vh 0.75vw;
+
+  cursor: pointer;
+  font-family: 'Roboto';
+  font-size: 0.7vw;
+  font-weight: bold;
+  color: #24274A;
+  height: 4vh;
+  width: 5.3vw;
+  margin: 2vh 1vh 0 37.5vw;
+  border: 1px solid;
+  background-color: white;
+  border-radius: 0.8rem;
+}
+.profilebutton input[type="file"] { /*파일 필드 숨기기*/
+  position: absolute; 
+  width: 1px; 
+  height: 1px; 
+  padding: 0; 
+  margin: -1px; 
+  overflow: hidden; 
+  clip:rect(0,0,0,0); border: 0;
+}
+
+.profilebutton .imgadd {
   cursor: pointer;
   font-family: 'Roboto';
   font-size: 0.7vw;
   font-weight: bold;
   color: white;
   height: 4vh;
-  width: 5vw;
-  margin: 2vh 0 0 40.5vw;
+  width: 5.3vw;
+  margin: 2vh 0 0 0;
   background-color: #24274A;
   border-radius: 0.8rem;
+}
+
+.progress {
+  margin: 1vh 1vh 0 41.5vw;
+  font-weight: bold;
 }
 
 .nicknamelabel {
