103f2a0e17d524c02ddd294f5132336533f05da3
JS <zkxp1215@naver.com>
2022-02-10 10:30:11 +0900

    feature/BE_UserUpdateDto_수정

--- backend/src/main/java/com/ssafy/dtod/dto/UserUpdateDto.java
+++ backend/src/main/java/com/ssafy/dtod/dto/UserUpdateDto.java
@@ -2,10 +2,6 @@ package com.ssafy.dtod.dto;
 
 import java.util.List;
 
-import javax.validation.constraints.NotNull;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,19 +15,14 @@ import lombok.Setter;
 @NoArgsConstructor
 public class UserUpdateDto {
 	
-	@NotNull
 	private String userEmail;
 
-	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-	@NotNull
 	private String userPwd;
 
-	@NotNull
 	private String userName;
 
 	private List<String> userTechstack;
 
-	@NotNull
 	private String userJobs;
 	
 	private String userImg;
