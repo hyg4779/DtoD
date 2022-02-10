e99fa870fb8ca8c4d64043f571ef1b1d9a055972
LEE DONG CHUL <sok8079@naver,com>
2022-02-10 21:55:24 +0900

    feat: FE_check

--- frontend/api.js
+++ frontend/api.js
@@ -4,6 +4,8 @@ export const api = {
   USER_INFO_GET: 'http://localhost:9090/api/user/mypage',
   USER_INFO_CHANGE: 'http://localhost:9090/api/user/update',
   OTHER_USER_INFO_GET: 'http://localhost:9090/api/user/',
+  NICKNAME_CHECK: 'http://localhost:9090/api/user/checkname/',
+  EMAIL_CHECK: 'http://localhost:9090/api/user/checkemail/',
 
   CREATE_STUDY_ROOM: 'http://localhost:9090/',
   
