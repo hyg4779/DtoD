cc979d6ed7416330786030ead15d38baf16d53d0
LEE DONG CHUL <sok8079@naver,com>
2022-02-14 10:38:28 +0900

    feat: FE_study_manage

---
+++ frontend/src/components/profile/editstudy/EditInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="editinfo">
+    <div class="title">
+      스터디룸 정보 수정
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditInfo'
+}
+</script>
+
+<style scoped>
+
+.editinfo .title {
+  font-size: 1.58vw;
+  margin: 2.5vh 0 0 1vw;
+  font-weight: bold;
+  color: #0D1350;
+}
+
+</style>
